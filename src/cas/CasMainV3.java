1e43c8deda3c1cbd586850f49eb13f35da461f7f
이상규 <76906458+leeeesanggyu@users.noreply.github.com>
2024-11-23 12:00:41 +0900

    CAS 연산 예제

---
+++ src/cas/CasMainV3.java
@@ -0,0 +1,56 @@
+package cas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static utils.Logger.log;
+import static utils.ThreadUtil.sleep;
+
+public class CasMainV3 {
+
+    private static final int THREAD_COUNT = 2;
+
+    public static void main(String[] args) throws InterruptedException {
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        System.out.println("start value = " + atomicInteger.get());
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                incrementAndGet(atomicInteger);
+            }
+        };
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Thread thread = new Thread(runnable);
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread: threads) {
+            thread.join();
+        }
+
+        int result = atomicInteger.get();
+        System.out.println(atomicInteger.getClass().getSimpleName() + " resultValue: " + result);
+    }
+
+    /**
+     * CAS 방식은 낙관적 접근법
+     */
+    private static int incrementAndGet(AtomicInteger atomicInteger) {
+        int getValue;
+        boolean result;
+        do {
+            getValue = atomicInteger.get();
+            sleep(100);
+            log("getValue: " + getValue);
+            result = atomicInteger.compareAndSet(getValue, getValue + 1);
+            log("result: " + result);
+        } while (!result);
+
+        return getValue + 1;
+    }
+}
